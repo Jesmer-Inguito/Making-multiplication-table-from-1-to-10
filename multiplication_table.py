bed5cf750f4bbc502ae6ad8accab1cf17f7343b5
unknown <you@example.com>
2024-01-26 14:18:54 +0800

    table created

--- multiplication_table.py
+++ multiplication_table.py
@@ -3,6 +3,7 @@
 # Create a loop using range()
 for i in range(1, 11):
     for j in range(1, 11):
-        print(i * j, sep= " ")
-
+        print(i * j, end= " ")
+        
 # Print results
+    print("\t\t")
